d3f2e7622650f4ea327c5276d950ca0eaa98d35f
Freddie Preece <freddie@muzmatch.com>
2020-05-29 16:50:50 +0100

    Add route and cors

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 9000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+let testAPIRouter = require("./routes/testAPI");
+let cors = require("cors");
+
+app.use(cors());
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/', (req, res) => res.send('Hello World!'))
+app.use("/testAPI", testAPIRouter);
+
+
