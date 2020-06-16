df772572237551a1079b26cad939229eb8d04ebc
Freddie Preece <freddie@muzmatch.com>
2020-06-16 17:37:01 +0100

    Add env for port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 9000
+const port = process.env.PORT || 9000;
 
 let testAPIRouter = require("./routes/testAPI");
 let cors = require("cors");
@@ -10,5 +10,3 @@ app.use(cors());
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use("/testAPI", testAPIRouter);
-
-
